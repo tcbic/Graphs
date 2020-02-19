75d28b14ee837897be115522b5a95e137cd213c7
Taylor <bickell.taylor@gmail.com>
2020-02-19 10:53:38 -0800

    Day 3

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -38,35 +38,90 @@ class Graph:
 # 2) Most economical in the long run (i.e. dveloper to time) to
 # just build out the graph.
 
+# def earliest_ancestor(ancestors, starting_node):
+#     # Build the graph.
+#     graph = Graph()
+#     for pair in ancestors:
+#         graph.add_vertex(pair[0])
+#         graph.add_vertex(pair[1])
+
+#         # Build edges in reverse.
+#         graph.add_edge(pair[1], pair[0])
+
+#     # Perform BFS. (Storing the path.)
+#     queue = Queue()
+#     queue.enqueue([starting_node])
+#     max_path_len = 1
+#     earliest_ancestor = -1
+#     while queue.size() > 0:
+#         path = queue.dequeue()
+#         current_node = path[-1]
+
+#         # If the path is longer or equal and the value is smaller,
+#         # or if the path is longer:
+#         # DO THE THING
+#         if (len(path) >= max_path_len and current_node < earliest_ancestor) or (len(path) > max_path_len):
+#             earliest_ancestor = current_node
+#             max_path_len = len(path)
+
+#         for neighbor in graph.vertices[current_node]:
+#             path_copy = list(path)
+#             path_copy.append(neighbor)
+#             queue.enqueue(path_copy)
+
+#     return earliest_ancestor
+
+# 3 steps to solving this:
+# 1) Describe the problem in terms of graphs.
+# 2) 
+# 3) 
+
+# Grab the graph class.
+
+
 def earliest_ancestor(ancestors, starting_node):
-    # Build the graph.
+
+    # Make a graph.
     graph = Graph()
+
     for pair in ancestors:
-        graph.add_vertex(pair[0])
-        graph.add_vertex(pair[1])
+        parent = pair[0]
+        child = pair[1]
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        # Add an edge from the child to the parent.
+        graph.add_edge(child, parent)
 
-        # Build edges in reverse.
-        graph.add_edge(pair[1], pair[0])
+    # BFS
 
-    # Perform BFS. (Storing the path.)
+    # Note: Determined that we could just return the last node
+    # to be visited.
+    # What if our input node is 8? Should return 4.
+    # What if our input is 11? Should return -1.
+
     queue = Queue()
+    # Path of the starting node.
     queue.enqueue([starting_node])
-    max_path_len = 1
+
+    longest_path_length = 1
     earliest_ancestor = -1
+
     while queue.size() > 0:
         path = queue.dequeue()
         current_node = path[-1]
 
-        # If the path is longer or equal and the value is smaller,
-        # or if the path is longer:
-        # DO THE THING
-        if (len(path) >= max_path_len and current_node < earliest_ancestor) or (len(path) > max_path_len):
+        if len(path) >= longest_path_length and current_node < earliest_ancestor:
+            longest_path_length = len(path)
             earliest_ancestor = current_node
-            max_path_len = len(path)
 
-        for neighbor in graph.vertices[current_node]:
+        if len(path) > longest_path_length:
+            longest_path_length = len(path)
+            earliest_ancestor = current_node
+
+        neighbors = graph.vertices[current_node]
+        for ancestor in neighbors:
             path_copy = list(path)
-            path_copy.append(neighbor)
+            path_copy.append(ancestor)
             queue.enqueue(path_copy)
-
+    
     return earliest_ancestor
